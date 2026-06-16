da636594c3b53a56cb8f0df2ed6d18c7a951fd9e
rtocastro <ricktorres@live.com>
2026-06-15 21:26:28 -0700

    added mock data to represent portfolio

---
+++ src/data/mockPortfolio.js
@@ -0,0 +1,20 @@
+const portfolio = [
+  {
+    id: 1,
+    ticker: "VOO",
+    shares: 5,
+    costBasis: 450,
+    currentPrice: 520,
+    ethicalScore: 92,
+  },
+  {
+    id: 2,
+    ticker: "SCHD",
+    shares: 10,
+    costBasis: 70,
+    currentPrice: 84,
+    ethicalScore: 88,
+  },
+];
+
+export default portfolio;
