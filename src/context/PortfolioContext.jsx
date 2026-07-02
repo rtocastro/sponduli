fb9159b607b93f2bfc80d494bbc86f80f3ba6e3e
rtocastro <ricktorres@live.com>
2026-07-01 23:58:22 -0700

    removed old local funtion for buildportfromtransact

--- src/context/PortfolioContext.jsx
+++ src/context/PortfolioContext.jsx
@@ -9,38 +9,7 @@ import {
 
 const PortfolioContext = createContext();
 
-function buildPortfolioFromTransactions(transactions = []) {
-  const holdingsMap = {};
 
-  transactions.forEach((transaction) => {
-    if (transaction.type !== "BUY") return;
-
-    if (!holdingsMap[transaction.ticker]) {
-      holdingsMap[transaction.ticker] = {
-        id: transaction.ticker,
-        ticker: transaction.ticker,
-        name: transaction.name,
-        shares: 0,
-        totalInvested: 0,
-        ethicalScore: transaction.ethicalScore || 80,
-        source: transaction.source,
-        transactions: [],
-      };
-    }
-
-    holdingsMap[transaction.ticker].shares += Number(transaction.shares || 0);
-    holdingsMap[transaction.ticker].totalInvested += Number(transaction.amount || 0);
-    holdingsMap[transaction.ticker].transactions.push(transaction);
-  });
-
-  return Object.values(holdingsMap).map((holding) => ({
-    ...holding,
-    costBasis:
-      holding.shares > 0 ? holding.totalInvested / holding.shares : 0,
-    currentPrice:
-      holding.shares > 0 ? holding.totalInvested / holding.shares : 0,
-  }));
-}
 
 export function PortfolioProvider({ children }) {
   const { transactions } = useTransactions();
