35ade2a6518a192b4a7009414d9d18e0329a51e2
rtocastro <ricktorres@live.com>
2026-07-05 22:03:41 -0700

    cleaned up providers list and got timeline provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { PortfolioProvider } from "./context/PortfolioContext";
 import { SettingsProvider } from "./context/SettingsContext";
-import { TimelineProvider } from "./context/TimelineContext";
 import { TransactionProvider } from "./context/TransactionContext";
 import "./index.css";
 import App from "./App.jsx";
@@ -12,13 +11,11 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <SettingsProvider>
-        <TimelineProvider>
-          <TransactionProvider>
-            <PortfolioProvider>
-              <App />
-            </PortfolioProvider>
-          </TransactionProvider>
-        </TimelineProvider>
+        <TransactionProvider>
+          <PortfolioProvider>
+            <App />
+          </PortfolioProvider>
+        </TransactionProvider>
       </SettingsProvider>
     </BrowserRouter>
   </StrictMode>
